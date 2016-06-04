b73a29a32b64cb5bde1cd049ebdd658cdb79a4db
takw2477 <birdsurachai@gmail.com>
2016-06-04 16:08:08 +0700

    gasgaga

--- views/UserController/logout.php
+++ views/UserController/logout.php
@@ -2,4 +2,4 @@
 unset($_SESSION['type']);
 ?>
 
- <meta http-equiv="refresh" content="0; url=index.php?controllers=SiteController&views=index">
+ <meta http-equiv="refresh" content="0; url=index.php?controllers=SiteController&views=project">
